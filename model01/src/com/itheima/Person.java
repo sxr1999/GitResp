00705fc541439cc2533773b8febc027fa8820a72
sxr1999 <shenxinran1999@gmail.com>
2021-08-26 23:21:40 +0800

    在person添加了一个属性

---
+++ model01/src/com/itheima/Person.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+/**
+ * @program: GitTest
+ * @description:
+ * @packagename: com.itheima
+ * @author: 沈欣然
+ * @date: 2021-08-26 23:20
+ **/
+public class Person {
+    private int age;
+}
