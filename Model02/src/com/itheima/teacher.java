e1762e332f4330548b6947cadbb91d37a5feab6a
shenxinran <shenxinran1999@gmail.com>
2021-08-26 23:15:23 +0800

    写了一行代码

---
+++ Model02/src/com/itheima/teacher.java
@@ -0,0 +1,16 @@
+package com.itheima;
+
+/**
+ * @program: GitTest
+ * @description
+ * @author: ShenXinran
+ * @create: 2021-08-26 23:12
+ **/
+public class teacher {
+    public static void main(String[] args) {
+        int i = 1;
+        int b = 5;
+        System.out.println(i+b);
+    }
+
+}
